b5a6507d914919f243abb43ca61a554ec6c32a14
BrunnoLukas <42772613+BrunnoLukas@users.noreply.github.com>
2020-04-09 12:45:37 -0300

    casa segura ok, erro na casa entrada

--- CasaSegura.java
+++ CasaSegura.java
@@ -7,42 +7,55 @@
  */
 public class CasaSegura extends Casa
 {
-    private String corDaCasa;
+        private String corDaCasa;
 	private int movimentos;
 
-	public CasaSegura(String corDaZonaSegura,Casa casa) {
-		super();
-		corDaCasa = corDaZonaSegura;
-	}
+    	public CasaSegura(String cor, Casa anterior) {
+            super(cor, anterior);
+        }
 
-	public Casa proximaCasa(Castelo castelo, boolean curupira, Dado[] dados) {
-		if (castelo.getNivel() == 1)
-			movimentos = dados[0].getValor() + dados[1].getValor();
-		else
-			movimentos = Math.min(dados[0].getValor(), dados[1].getValor());
 
-		if (castelo.getCor() == corDaCasa) {
-			return super.getCasaSegura().proximaCasa(castelo, curupira, movimentos - 1);
-		}
-
-		return super.getCasaSeguinte().proximaCasa(castelo, curupira, movimentos - 1);
-	}
-
-	public Casa proximaCasa(Castelo castelo, boolean curupira, int casasAAndar) {
-
-		if (this.possuiPeca()) {
-			if (this.getPeca().getNivel() > castelo.getNivel() && this.getPeca().getCor() != castelo.getCor())
-				return super.getCasaAnterior();
-		}
-
-		if (casasAAndar > 0) {
-			if (castelo.getCor() == corDaCasa) {
-				return super.getCasaSegura().proximaCasa(castelo, curupira, casasAAndar - 1);
-			}
-
-			return super.getCasaSeguinte().proximaCasa(castelo, curupira, casasAAndar - 1);
-		}
-
-		return this;
-	}
+    	public Casa proximaCasa(Castelo castelo, boolean curupira, Dado[] dados) {
+            if (castelo.getNivel() == 1)
+                movimentos = dados[0].getValor() + dados[1].getValor();
+            else
+                movimentos = Math.min(dados[0].getValor(), dados[1].getValor());
+    
+            if (curupira) {
+                if (super.getCasaAnterior() != null)
+                    return super.getCasaAnterior().proximaCasa(castelo, curupira, movimentos - 1);
+                else {
+                    curupira = false;
+                    return super.getCasaSeguinte().proximaCasa(castelo, curupira, movimentos - 1);
+                }
+            }
+    
+            if (super.ehCasaFinal()) {
+                return this;
+            }
+    
+            return super.getCasaSeguinte().proximaCasa(castelo, curupira, movimentos - 1);
+        }
+    
+        public Casa proximaCasa(Castelo castelo, boolean curupira, int casasAAndar) {
+            if (casasAAndar > 0) {
+                if (curupira) {
+                    if (super.getCasaAnterior() != null)
+                        return super.getCasaAnterior().proximaCasa(castelo, curupira, casasAAndar - 1);
+                    else {
+                        curupira = false;
+                        return super.getCasaSeguinte().proximaCasa(castelo, curupira, casasAAndar - 1);
+                    }
+                }
+    
+                if (super.ehCasaFinal()) {
+                    curupira = true;
+                    return super.getCasaAnterior().proximaCasa(castelo, curupira, casasAAndar - 1);
+                }
+    
+                return super.getCasaSeguinte().proximaCasa(castelo, curupira, casasAAndar - 1);
+            }
+    
+            return this;
+        }
 }
